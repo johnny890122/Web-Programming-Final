0efef1ac362fe781055ba50557e6351baa1b2a13
johnny890122 <j087061439@gmail.com>
2021-12-06 03:04:24 +0800

    initail draggable block

---
+++ block/src/App.js
@@ -0,0 +1,13 @@
+import Draggable from "react-draggable";
+
+function App() {
+  return (
+  <Draggable>
+    <div>I can now be moved around!</div>
+  </Draggable>
+  );
+}
+
+export default App;
+
+
