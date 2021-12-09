6b0bc9850ded0fa689de3b592e958884958c36d3
Yoga096 <b06303096@ntu.edu.tw>
2021-12-10 05:00:23 +0800

    List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,18 @@ import './App.css';
 import { Fragment } from 'react';
 import Header from './containers/Header.js';
 import NavBar from './containers/NavBar.js';
-import Body from './containers/Body.js'
+import Body from './containers/Body.js';
+import ListUserTeam from './components/List';
 
 
 function App() {
 	return (
 		<>
-			<Header / >
-			<NavBar / >
-			<Body / >
+			<Header />
+			<NavBar />
+			<Body>
+        <ListUserTeam />
+      </Body>
 		</>
 	)
 }
