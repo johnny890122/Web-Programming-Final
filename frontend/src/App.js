bf75436824e859cce0e90c9710c336cc17285af9
Lynn Chien <moneychien.aom@gmail.com>
2021-12-10 14:49:11 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,17 @@
-<<<<<<< HEAD
-import logo from "./logo.svg";
 import "./App.css";
-import { Fragment } from "react";
-<<<<<<< HEAD
-
 import { useState } from "react";
 import UserDashboard from "./containers/UserDashboard";
 import LogIn from "./containers/LogIn";
-=======
-import './App.css';
-import Calendar from './Components/Calendar';
-import UserTeam from './Components/UserTeam';
-import UserAchievement from './Components/UserAchievement';
-import TeamHP from './Components/TeamHp';
-import TeamPost from './Components/TeamPost';
-import TeamScore from './Components/TeamScore';
-import TeamVote from './Components/TeamVote';
->>>>>>> 0f88246... .
-=======
-import Header from "./containers/Header.js";
-import NavBar from "./containers/NavBar.js";
-import Body from "./containers/Body.js";
-import ListUserTeam from './components/List';
->>>>>>> main
 
 function App() {
   const [logined, setLogined] = useState(false);
   return (
-<<<<<<< HEAD
     <section className="wrapper">
       {logined ? (
         <UserDashboard setLogined={setLogined} />
       ) : (
         <LogIn setLogined={setLogined} />
       )}
-=======
-    <section>
-      <Calendar/>
-      <UserTeam/>
-      <UserAchievement/>
-      <TeamHP/>
-      <TeamPost/>
-      <TeamScore/>
-      <TeamVote/>
->>>>>>> 0f88246... .
     </section>
   );
 }
