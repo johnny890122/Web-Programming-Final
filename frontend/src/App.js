3a4719aa4f193684cb780520c2ef3c4be51dd324
johnny890122 <j087061439@gmail.com>
2021-12-10 14:41:47 +0800

    merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import logo from "./logo.svg";
 import "./App.css";
 import { Fragment } from "react";
@@ -5,16 +6,37 @@ import { Fragment } from "react";
 import { useState } from "react";
 import UserDashboard from "./containers/UserDashboard";
 import LogIn from "./containers/LogIn";
+=======
+import './App.css';
+import Calendar from './Components/Calendar';
+import UserTeam from './Components/UserTeam';
+import UserAchievement from './Components/UserAchievement';
+import TeamHP from './Components/TeamHp';
+import TeamPost from './Components/TeamPost';
+import TeamScore from './Components/TeamScore';
+import TeamVote from './Components/TeamVote';
+>>>>>>> 0f88246... .
 
 function App() {
   const [logined, setLogined] = useState(false);
   return (
+<<<<<<< HEAD
     <section className="wrapper">
       {logined ? (
         <UserDashboard setLogined={setLogined} />
       ) : (
         <LogIn setLogined={setLogined} />
       )}
+=======
+    <section>
+      <Calendar/>
+      <UserTeam/>
+      <UserAchievement/>
+      <TeamHP/>
+      <TeamPost/>
+      <TeamScore/>
+      <TeamVote/>
+>>>>>>> 0f88246... .
     </section>
   );
 }
---
+++ frontend/src/components/UserAchievement.js
@@ -0,0 +1,19 @@
+function UserAchievement() {
+    return (
+      <>
+        <h2>Achievement</h2>
+        <ul className = "user-achievement-list">
+            <li className = "user-achievement-item">
+                <h3>Exercise</h3>
+            </li>
+            <li className = "user-achievement-item">
+                <h3>Habbit</h3>
+            </li>
+            <li className = "user-achievement-item">
+                <h3>Productivity</h3>
+            </li>
+        </ul>
+      </>
+    );
+  };
+export default UserAchievement
