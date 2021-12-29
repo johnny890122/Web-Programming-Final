8da751c5f5217916ac69d307ea618fd975386219
Yoga096 <46023776+Yoga096@users.noreply.github.com>
2021-12-29 15:21:59 +0800

    Add files via upload

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,20 @@
 import { GraphQLServer, PubSub } from "graphql-yoga";
 
 // resolvers
-import Query from "./resolvers/Query.js";
-import Mutation from "./resolvers/Mutation.js";
-import Subscription from "./resolvers/Subscription.js";
+//import Query from "./resolvers/Query.js";
+//import Mutation from "./resolvers/Mutation.js";
+//import Subscription from "./resolvers/Subscription.js";
+
+const { typeDefs } = require('./tempTypeDefs') // 改掉
+const { resolvers } = require("./tempResolvers"); // 改掉
 // db
 // import taskModel from "./models/task.js";
 
 const port = process.env.PORT || 5000;
 const pubSub = new PubSub();
 const server = new GraphQLServer({
-  typeDefs: "./src/schema.graphql",
-  resolvers: {
-    Query,
-    Mutation,
-    Subscription,
-  },
+  typeDefs,
+  resolvers,
   context: {
     // taskModel,
     pubSub,
