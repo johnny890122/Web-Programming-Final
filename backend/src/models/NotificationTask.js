11a463f25130c7ec4f9bd836b682fa5135af4bbb
johnny890122 <j087061439@gmail.com>
2022-01-06 15:46:03 +0800

    merge money

--- backend/src/models/NotificationTask.js
+++ backend/src/models/NotificationTask.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const NotificationTaskSchema = new Schema({
-	taskID:{type: String, required: true},	taskType: {type: String, required: true},
+	taskID: {type: String, required: true},
+	taskType: {type: String, required: true},
 	taskContent: {type: String, required: true},
 })
 
