1c182f58aa6c0fa21303c0e021cc8b1d98b0fa0e
Yoga096 <b06303096@ntu.edu.tw>
2022-01-11 12:41:08 +0800

    postModal

--- frontend/src/containers/TeamPost.js
+++ frontend/src/containers/TeamPost.js
@@ -1,17 +1,113 @@
 import React from "react";
+import { useState, useRef, useEffect } from "react";
 import Template from "../components/Template";
 import { PostData} from "../components/ListData";
-import { List, ListItem, Typography, Card, Box, Button, Grid, CardContent, Chip } from '@mui/material';
+import { List, ListItem, Typography, Card, Box, CardContent } from '@mui/material';
 import { CardActionArea } from '@mui/material';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import PostAddIcon from '@mui/icons-material/PostAdd';
 import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
+import { Modal, Button, Form, Input } from 'antd';
 
 /* 連結detail頁面 */
 
 function TeamPost() {
 
+  const formLayout = {
+    labelCol: {
+      span: 8,
+    },
+    wrapperCol: {
+      span: 16,
+    },
+  };
+
   const posts = PostData;
+  const [postNow, setPostNow] = useState(null)
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isEdit, setIsEdit] = useState(false)
+
+  const showModal = (post) => {
+    setPostNow(post)
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+    setIsEdit(false);
+  };
+
+  const handleEdit = () => {
+    setIsEdit(true);
+  };
+
+  const handleBack = () => {
+    setIsEdit(false);
+  };
+
+  const onSubmit = () => {
+    console.log('Success');
+  };
+
+  const postDetail = (
+    <>
+      <Typography variant="h3" sx={{ m: 1  }}>
+        {postNow ? postNow.title: ""}
+      </Typography>
+      <Typography variant="h6" sx={{ m: 1 }}>
+        作者: {postNow ? postNow.author: ""}
+      </Typography>
+      <Typography variant="h5" sx={{ m: 1 }}>
+        {postNow ? postNow.content: ""}
+      </Typography>
+    </>
+  )
+
+  const postForm = (
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      initialValues={{ title: postNow.title, content: postNow.content }}
+      onFinish={onSubmit}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Title"
+        name="title"
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Content"
+        name="content"
+      >
+        <Input.TextArea size="large"/>
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  )
+
+  const postModal = (
+    <Modal title="Post" visible={isModalVisible}
+           onOk={handleOk} onCancel={handleOk}
+           confirmLoading={isEdit}
+           footer={isEdit? [
+              <Button key="back" onClick={handleBack}>Back</Button>,
+              <Button key="ok" onClick={handleOk}>OK</Button>
+            ] : [
+              <Button key="edit" onClick={handleEdit}>Edit</Button>,
+              <Button key="ok" onClick={handleOk}>OK</Button>
+            ]}>
+      {isEdit ? postForm : postDetail}     
+    </Modal>
+  )
 
   const postlist = ( 
     <Box className = "team-post"
@@ -19,6 +115,7 @@ function TeamPost() {
       <Typography display= 'inline' variant="h3" component="div" sx={{ m:2, width: '100%', textAlign: "left" }}>
             <PostAddIcon sx={{ mx: 1 }} fontSize="large"/> Post
       </Typography>
+      {postModal}
       <List className = "team-post-list" sx={{ p:1, width: '100%' }}>
           <ListItem button key = "0">
               <Card sx={{ m: 1, p:1, width: '100%' }}>
@@ -34,10 +131,10 @@ function TeamPost() {
                   </CardActionArea>
               </Card>
           </ListItem>
-          {posts.map(post => <ListItem button key = {post.id}>
-                                <Card sx={{ m: 1, p:1, width: '100%' }}>
-                                    <CardActionArea sx={{ width: '100%' }}
-                                                          href={`#post-detail`}>
+          {posts.map(post => <ListItem key = {post.id}>
+                              <Card sx={{ m: 1, p:1, width: '100%' }}>
+                                    <CardActionArea sx={{ width: '100%' }} onClick={() => {showModal(post)}}
+                                                    >
                                       <CardContent sx={{ p: 2 }}>
                                         <Box sx={{ m:1, p: 1 }}>
                                           <Typography gutterBottom variant="h4" component="div" >
