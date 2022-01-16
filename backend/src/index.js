afd96dee1eb6d112cdc94b1bd5bc6bb1cd5524e5
johnny890122 <j087061439@gmail.com>
2022-01-16 15:18:24 +0800

    deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,6 +17,16 @@ import DetailPlayer from "./resolvers/DetailPlayer";
 // import Subscription from "./resolvers/Subscription";
 // db
 import * as db from "./db";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/*", function(req, res){
+  res.sendFile(path.join(__dirname, "build", "index.html"))
+})
+
+
 
 const pubSub = new PubSub();
 const server = new GraphQLServer({
