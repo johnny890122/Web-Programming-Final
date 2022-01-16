f72291a12b53ea3c6e7cc7fc2cd22fe4140adcc7
johnny890122 <j087061439@gmail.com>
2022-01-16 15:25:09 +0800

    deploy

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+/* webpack.config.js ： Webpack 的設定檔 */
+const path = require('path');
+const clientConfig = {
+  entry: {
+    'index': './app.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    // 獲取絕對路徑的方法
+    filename: '[name].bundle.js'
+  }
+}
+module.exports = [clientConfig];
